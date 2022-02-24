3282dc1674078d0ab48025fa1f88acf84ecf3961
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-24 12:59:53 -0500

    commit tree 499bcd35fe7c39e1397a3795cee49570f93d0721

---
+++ src/components/Band.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Band extends Component {
+
+  render() {
+    return(
+      <li>{this.props.band.name} <button onClick={() => this.props.delete(this.props.band.id)}>DELETE</button></li>
+    );
+  }
+};
+
+export default Band;
